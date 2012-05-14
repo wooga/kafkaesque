f5629c9772db18f0acd99fe7962fe39e6a1771fc
Gerrit Riessen <gerrit.riessen@gmail.com>
2012-05-14 12:36:27 +0200

    update the VATs for those countries that have VAT
    refs #3
    
    On branch: issue_3_Correct_Bookings_Calculations_i

--- test/test_bookings.rb
+++ test/test_bookings.rb
@@ -3,8 +3,8 @@ require 'helper'
 class TestBookings < Test::Unit::TestCase
   context "calculate" do
     should "use the correct formula" do
-      assert_equal 5.40, Kafkaesque::Bookings.calculate(100.0, 'de')
-      assert_equal 5.40, Kafkaesque::Bookings.calculate(100.0, 'gb')
+      assert_equal 5.7, Kafkaesque::Bookings.calculate(100.0, 'de')
+      assert_equal 5.7, Kafkaesque::Bookings.calculate(100.0, 'gb')
       assert_equal 7.0, Kafkaesque::Bookings.calculate(100.0, 'ch')
       assert_equal 7.0, Kafkaesque::Bookings.calculate(100.0, 'xx')
     end
